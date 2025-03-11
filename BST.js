1b69a4f5918c57d5b9b29c7927eb131d74a0b006
tot121212 <totmusicbusiness@gmail.com>
2025-03-11 15:40:03 -0500

    finished

--- BST.js
+++ BST.js
@@ -9,10 +9,14 @@ class Node{
 // balanced binary tree
 export class Tree{
     constructor(array){
+        this.init(array);
+    }
+
+    init(array){
         const sorted = this.process(array);
-        console.log("Sorted Array:",sorted);
+        //console.log("Sorted Array:",sorted);
         this.root = this.buildTree(sorted, 0, sorted.length-1);
-        console.log("Root built:", this.root);
+        //console.log("Root built:", this.root);
     }
 
     prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -45,88 +49,94 @@ export class Tree{
 
     //[1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
     //[1,3,4,5,7,8,9,23,67,324,6345]
-    insert(value, node = this.root){
+    insert(value){
         if (!this.root){
             this.root = new Node(value);
             return true;
         }
-        if (value === node.data) return false;
-        let nextNode, isRight;
-
-        if (value < node.data) {
-            nextNode = node.left; 
-            isRight = false;
-        }
-        else if (value > node.data) {
-            nextNode = node.right; 
-            isRight = true;
-        }
-
-        if (!(nextNode instanceof Node)) {
-            nextNode = new Node(value);
-            isRight ? node.right = nextNode : node.left = nextNode;
-        } else {
-            this.insert(value, nextNode);
-        }
-        return true;
-    }
-
-    deleteItem(value, node = this.root, parent = null){
-        if (!(node instanceof Node)) return false; // node does not exist
-        if (value === node.data){
-            if (!node.left && !node.right) { // no children
-                if (parent){
-                    parent.left === node ? parent.left = null : parent.right = null; // null out parent left or right
-                } else {
-                    this.root = null;
+        const recurse = (value, node, parent, side)=>{
+            if (!(node instanceof Node)){
+                node = new Node(value);
+                if (parent instanceof Node){
+                    if (side === "l"){
+                        parent.left = node;
+                        console.log("Inserting",value);
+                        return true;
+                    } else if (side === "r"){
+                        parent.right = node;
+                        console.log("Inserting",value);
+                        return true;
+                    }
                 }
-            } else if (node.left && node.right){ // both children
-                let nextLargest = node.right; // start with right node
-                let nextLargestParent = node; // store parent of that node
-                while (nextLargest.left){ // search through left nodes until left is null
-                    nextLargestParent = nextLargest; // set current next largest to the parent
-                    nextLargest = nextLargest.left; // set nextLargest to actual nextLargest
-                }
-                if (nextLargest.right) nextLargestParent.left = nextLargest.right; // if that node has a right, make its right into the left child of the closestLargest instead of node itself
-                node.data = nextLargest.data; // now we have a decoupled nextLargest node, use that to set data of node
-            } else { // only one child
-                const child = node.left || node.right;
-                if (!parent) this.root = child;
-                else (parent.left === node) ? parent.left = child : parent.right = child;
+                return new Error("Trying to insert new value onto null parent");
             }
-            return true;
-        } 
-        else if (value < node.data){
-            return this.deleteItem(value, node.left, node);
-        } 
-        else { // value > node.data
-            return this.deleteItem(value, node.right, node);
+            if (value === node.data) return new Error("Value already exists in tree");
+            if (value < node.data) {
+                return recurse(value, node.left, node, "l");
+            } else {
+                parent = node;
+                return recurse(value, node.right, node, "r");
+            }
         }
+        return recurse(value, this.root, null, null);
     }
 
-    find(value, node = this.root){
-        if (!node) return null;
-        if (value === node.data) return node;
-        else if (value < node.data) return this.find(value, node.left);
-        else return this.find(value, node.right);
+    deleteItem(value){
+        const recurse = (value, node, parent)=>{
+            if (!(node instanceof Node)) return false; // node does not exist
+            if (value === node.data){
+                if (!node.left && !node.right) { // no children
+                    if (parent){
+                        parent.left === node ? parent.left = null : parent.right = null; // null out parent left or right
+                    } else {
+                        this.root = null;
+                    }
+                } else if (node.left && node.right){ // both children
+                    let nextLargest = node.right; // start with right node
+                    let nextLargestParent = node; // store parent of that node
+                    while (nextLargest.left){ // search through left nodes until left is null
+                        nextLargestParent = nextLargest; // set current next largest to the parent
+                        nextLargest = nextLargest.left; // set nextLargest to actual nextLargest
+                    }
+                    if (nextLargest.right) nextLargestParent.left = nextLargest.right; // if that node has a right, make its right into the left child of the closestLargest instead of node itself
+                    node.data = nextLargest.data; // now we have a decoupled nextLargest node, use that to set data of node
+                } else { // only one child
+                    const child = node.left || node.right;
+                    if (!parent) this.root = child;
+                    else (parent.left === node) ? parent.left = child : parent.right = child;
+                }
+                return true;
+            } 
+            else if (value < node.data){
+                return recurse(value, node.left, node);
+            } 
+            else { // value > node.data
+                return recurse(value, node.right, node);
+            }
+        }
+        return recurse(value, this.root, null);
+    }
+
+    find(value){
+        const recurse = (value, node)=>{
+            if (!node) return null;
+            if (value === node.data) return node;
+            else if (value < node.data) return recurse(value, node.left);
+            else return recurse(value, node.right);
+        }
+        return recurse(value, this.root);
     }
     
     // call a callback on each node in the tree, in level order traversal
     // lets try iteration for this one
-    levelOrder(callback){
+    levelOrder(callback) {
         if (!callback) throw new Error("No callback provided");
-        const queue = [this.root, null];
-        while (queue.length > 1){
-            let node = queue.shift();
-            if (node === null) {
-                console.log("Delimiter reached");
-                callback(null);
-                queue.push(null);
-                continue;
-            }
-            callback(node);
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
+        const queue = [[this.root, 0]];
+        while (queue.length) {
+            let [node, level] = queue.shift();
+            if (callback(node, level)) return;
+            if (node.left) queue.push([node.left, level + 1]);
+            if (node.right) queue.push([node.right, level + 1]);
         }
     }
 
@@ -135,7 +145,7 @@ export class Tree{
         if (!callback) throw new Error("No callback provided");
         const recurse = (node)=>{
             if (node.left) recurse(node.left);
-            callback(node);
+            if (callback(node)) return;
             if (node.right) recurse(node.right);
         };
         recurse(this.root);
@@ -144,7 +154,7 @@ export class Tree{
     preOrder(callback){
         if (!callback) throw new Error("No callback provided");
         const recurse = (node)=>{
-            callback(node);
+            if (callback(node)) return;
             if (node.left) recurse(node.left);
             if (node.right) recurse(node.right);
         };
@@ -156,16 +166,10 @@ export class Tree{
         const recurse = (node)=>{
             if (node.left) recurse(node.left);
             if (node.right) recurse(node.right);
-            callback(node);
+            if (callback(node)) return;
         };
         recurse(this.root);
     }
-
-    //      1  (Height 2, Depth 0)  <- Root
-    //     / \
-    //    2   3  (Height 1, Depth 1)
-    //   / \   \
-    //  4   5   6  (Height 0, Depth 2)
 
     height(node){
         if (!node) return -1;
@@ -176,22 +180,49 @@ export class Tree{
     }
 
     // the number of edges in the path from given node to tree root
-    depth(node){
-        // search tree for node, saving the steps it took to get there
-        let depth =- 1;
-        const recurse = (n, currentDepth)=>{
-            if (n === null) return;
-            if (n === node){
-                depth = currentDepth;
-                return;
+    depth(targetNode){
+        if (!targetNode) return null;
+        let depth = 0;
+        const recurse = (node)=>{
+            if (!(node instanceof Node)) return null;
+            if (targetNode.data === node.data) return depth;
+            depth+=1;
+            if (targetNode.data < node.data) return recurse(node.left);
+            else if (targetNode.data > node.data) return recurse(node.right);
+        }
+        return recurse(this.root);
+    }
+
+    //      1  (Height 2, Depth 0)  <- Root
+    //     / \
+    //    2   3  (Height 1, Depth 1)
+    //   / \   \
+    //  4   5   6  (Height 0, Depth 2)
+    //            7
+    //              8
+
+    // is the difference between heights of the left and right subtree of every node <= 1
+    isBalanced(){
+        const recurse = (node)=>{
+            if (node === null) return 0; // base case
+            let leftHeight = recurse(node.left); // left height is 2
+            if (leftHeight === -1) return -1;
+            let rightHeight = recurse(node.right); // right height is 4
+            if (rightHeight === -1) return -1;
+            if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+            else {
+                let height = Math.max(leftHeight, rightHeight) + 1;
+                //console.log(`${node.data} relative height is: ${height - 1}`);
+                return height;
             }
-            recurse(n.left, currentDepth + 1);
-            recurse(n.right, currentDepth + 1);
-        };
-        recurse(this.root, 0);
-        return depth
+        }
+        return (recurse(this.root) !== -1);
+    }
+
+    rebalance(){
+        console.log("Rebalancing tree...");
+        const array = [];
+        this.levelOrder((node, level)=>{array.push(node.data)});
+        this.init(array);
     }
 }
-
-
- 
--- main.js
+++ main.js
@@ -1,33 +1,101 @@
 import {Tree} from "./BST.js";
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-tree.insert(736);
-tree.insert(73366);
-let val = 35478;
-tree.insert(val);
+const getRandom = (min, max)=>{
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const randomNums = (min, max)=>{
+    const nums = [];
+    const amtOfNums = getRandom(3, 7);
+    for (let i = 0; i < amtOfNums; i++){
+        nums.push(getRandom(min, max));
+    }
+    return nums;
+}
+
+const tree = new Tree(randomNums(1, 100));
+
 tree.prettyPrint(tree.root);
-console.log(tree.deleteItem(val) ? `Node Deleted: ${val}` : "Node Not Found");
+console.log(tree.isBalanced() ? "Is balanced" : "Is not balanced");
+
+console.log("Level Order:");
+tree.levelOrder(node => console.log(node.data));
+
+console.log("\nInorder:");
+tree.inOrder(node => console.log(node.data));
+
+console.log("\nPreorder:");
+tree.preOrder(node => console.log(node.data));
+
+console.log("\nPostorder:");
+tree.postOrder(node => console.log(node.data));
+
+randomNums(1000, 10000).forEach((num)=>{
+    tree.insert(num);
+});
+
 tree.prettyPrint(tree.root);
-console.log(tree.find(val));
-console.log(tree.find(736));
+console.log(tree.isBalanced() ? "Is balanced" : "Is not balanced");
 
-// console.log("Level Order:");
-// tree.levelOrder(node => console.log(node.data));
-
-// console.log("\nInorder:");
-// tree.inOrder(node => console.log(node.data));
-
-// console.log("\nPreorder:");
-// tree.preOrder(node => console.log(node.data));
-
-// console.log("\nPostorder:");
-// tree.postOrder(node => console.log(node.data));
-
-console.log("Root Height:",tree.height(tree.root));
-console.log("736 Height:",tree.height(tree.find(736)));
-
-console.log("Root Depth:",tree.depth(tree.root));
-console.log("736 Depth:",tree.depth(tree.find(736)));
-
-console.log("\nTree Structure:");
+tree.rebalance();
 tree.prettyPrint(tree.root);
+console.log(tree.isBalanced() ? "Is balanced" : "Is not balanced");
+
+console.log("Level Order:");
+tree.levelOrder(node => console.log(node.data));
+
+console.log("\nInorder:");
+tree.inOrder(node => console.log(node.data));
+
+console.log("\nPreorder:");
+tree.preOrder(node => console.log(node.data));
+
+console.log("\nPostorder:");
+tree.postOrder(node => console.log(node.data));
+
+// const val1 = 35478;
+// const val2 = 736;
+// const val3 = 67;
+
+// tree.insert(val1);
+// tree.insert(val2);
+// console.log(tree.insert(val1).message ?? "");
+// tree.prettyPrint(tree.root);
+// console.log(tree.deleteItem(val1) ? `Node Deleted: ${val1}` : "Node Not Found");
+// tree.prettyPrint(tree.root);
+// console.log(`Find ${val1}:`,tree.find(val1));
+// console.log(`Find ${val2}:`,tree.find(val2));
+
+// // console.log("Level Order:");
+// // tree.levelOrder(node => console.log(node.data));
+
+// // console.log("\nInorder:");
+// // tree.inOrder(node => console.log(node.data));
+
+// // console.log("\nPreorder:");
+// // tree.preOrder(node => console.log(node.data));
+
+// // console.log("\nPostorder:");
+// // tree.postOrder(node => console.log(node.data));
+
+// console.log(`${tree.root.data} Height:`,tree.height(tree.root));
+// console.log(`${val2} Height:`,tree.height(tree.find(val2)));
+// console.log(`${val3} Height:`,tree.height(tree.find(val3)));
+
+// console.log(`${tree.root.data} Depth:`,tree.depth(tree.root));
+// console.log(`${val2} Depth:`,tree.depth(tree.find(val2)));
+// console.log(`${val3} Depth:`,tree.depth(tree.find(val3)));
+
+// tree.prettyPrint(tree.root);
+// console.log(tree.isBalanced() ? "Is balanced" : "Is not balanced");
+// console.log(tree.insert(323).message ?? "Inserted 323");
+
+// console.log("\nTree Structure:");
+// tree.prettyPrint(tree.root);
+// console.log(tree.isBalanced() ? "Is balanced" : "Is not balanced");
+
+// console.log(tree.insert(735).message ?? "Inserted 735");
+// console.log(tree.isBalanced() ? "Is balanced" : "Is not balanced");
+// tree.rebalance();
+// tree.prettyPrint(tree.root);
+// console.log(tree.isBalanced() ? "Is balanced" : "Is not balanced");
